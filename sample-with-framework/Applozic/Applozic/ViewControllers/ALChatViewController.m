2a0fd91b263b59767f542712fa3702534545680e
Jos van Velzen <jos@framuzz.com>
2017-12-26 12:19:46 +0100

    setup SDWebImage as framework and integrate with root carthage

--- sample-with-framework/Applozic/Applozic/ViewControllers/ALChatViewController.m
+++ sample-with-framework/Applozic/Applozic/ViewControllers/ALChatViewController.m
@@ -24,7 +24,7 @@
 #import "ALImageCell.h"
 #import "ALFileMetaInfo.h"
 #import "DB_FileMetaInfo.h"
-#import "UIImageView+WebCache.h"
+#import <SDWebImage/UIImageView+WebCache.h>
 #import "ALConnection.h"
 #import "ALConnectionQueueHandler.h"
 #import "ALRequestHandler.h"
