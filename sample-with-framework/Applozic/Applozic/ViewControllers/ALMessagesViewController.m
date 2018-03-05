dfe67049c1f6c97a2c7ea3925a91b45e4d791a6f
Jos van Velzen <jos@framuzz.com>
2018-03-05 17:07:19 +0100

    always refresh chatlist on viewWillAppear

--- sample-with-framework/Applozic/Applozic/ViewControllers/ALMessagesViewController.m
+++ sample-with-framework/Applozic/Applozic/ViewControllers/ALMessagesViewController.m
@@ -190,6 +190,10 @@
         [self.dBService getMessages:self.childGroupList];
         [self.detailChatViewController setRefreshMainView:FALSE];
         [self.mTableView reloadData];
+    }else{
+        [self.dBService getMessages:self.childGroupList];
+        [self.detailChatViewController setRefreshMainView:FALSE];
+        [self.mTableView reloadData];
     }
     
     if([ALUserDefaultsHandler isNavigationRightButtonHidden])
